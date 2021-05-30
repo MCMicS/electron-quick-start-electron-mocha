50b3dd8a09feecf31b3c22c833e64d041ae496b1
mcmics <MCMicS@users.noreply.github.com>
2021-05-30 19:54:48 +0200

    create sample

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Simple Test', function() {
+  describe('say Hello)', function() {
+    it('should success', function() {
+      assert.deepStrictEqual(true ,true);
+    });
+  });
+});
