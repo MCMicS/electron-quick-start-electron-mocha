6cf862bfe5e25c795a9c0e36971fb52c755b8f27
mcmics <MCMicS@users.noreply.github.com>
2021-05-31 18:57:50 +0200

    test Xvfb

--- test/testRenderer.js
+++ test/testRenderer.js
@@ -1,11 +1,10 @@
 var assert = require('assert');
 const electron = require("electron")
-const app = electron.app || electron.remote.app;
+const ipcRenderer = electron.ipcRenderer
 
 describe('Simple Test', function() {
   describe('say Hello from Renderer', function() {
     it('should success', function() {
-      console.log('App-data: ' + app.getPath('appData'))
       assert.deepStrictEqual(true ,true);
     });
   });
